2b8869f9de253a685bf900fb96a55d75de0e8eef
sdrawkcaBdeT <sdrawkcabdet@gmail.com>
2023-09-10 00:56:53 -0500

    notes

--- omega_strikers/object_detection_in_games.py
+++ omega_strikers/object_detection_in_games.py
@@ -1,7 +1,7 @@
 import cv2 as cv2
 import numpy as np
 
-haystack = cv2.imread('omega_strikers/scenario_01_img_02005.jpg')
+haystack = cv2.imread('omega_strikers/sc_02_img_02.jpg')
 grayhaystack = cv2.cvtColor(haystack, cv2.COLOR_BGR2GRAY)
 
 needle = cv2.imread('omega_strikers/os_core.jpg')
@@ -25,4 +25,15 @@ cv2.imshow('Result', result)
 cv2.imshow('output', output)
 cv2.waitKey(0)
 
-cv2.imwrite('haystack_needle_scen_01_02005.png', output)
+cv2.imwrite('haystack_needle_scen_02_02.png', output)
+
+# Might need different core for when it turns red
+# How to note when goal is scored
+# How to feed continuous streams of image for core tracking
+# How to mark events (saves, shots (crossing defined line))
+# How to deal with different maps
+# Initially - goal is to watch omega strikers game and tally proper score for each team - left and right.
+# Then layer in core location, user strikes, goals, and eventually ability usuage
+# How to build this into a system...
+# One though already is to have have same video processed with different 'needle' images (needles=All Strikers, to track striker location and sense who was in game. To do so would require a separate needle image of the striker to process the video, unless there's a way to sense mutiple objects in frame and denote they are different through the current framework)
+# 
