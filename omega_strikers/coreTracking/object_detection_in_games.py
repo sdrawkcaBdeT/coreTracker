0c598eabab9d807b478038d7558bc8c580f2dc04
sdrawkcaBdeT <sdrawkcabdet@gmail.com>
2023-09-10 16:45:15 -0500

    reorganize file structure, rename images more clearly, comment code

--- omega_strikers/coreTracking/object_detection_in_games.py
+++ omega_strikers/coreTracking/object_detection_in_games.py
@@ -1,14 +1,23 @@
 import cv2 as cv2
 import numpy as np
 
-haystack = cv2.imread('omega_strikers/sc_02_img_02.jpg')
+# Define haystack (image to find the needle in)
+haystack = cv2.imread('omega_strikers/coreTracker/img/haystacks/scenario00_haystack00.jpg')
+# grayscale transformation
 grayhaystack = cv2.cvtColor(haystack, cv2.COLOR_BGR2GRAY)
 
+# Define the needle (image of os core in this case)
 needle = cv2.imread('omega_strikers/os_core.jpg')
+# grayscale transformation
 grayneedle =cv2.cvtColor(needle, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Needle', needle)
-cv2.imshow('Haystack', haystack)
+# # Uncomment to show image
+# cv2.imshow('Needle', needle)
+# cv2.waitKey(0)
+
+# # Uncomment to show image
+# cv2.imshow('Haystack', haystack)
+# cv2.waitKey(0)
 
 #methods available :[cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TMO_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 result = cv2.matchTemplate(grayhaystack, grayneedle, cv2.TM_CCOEFF_NORMED)
@@ -20,11 +29,18 @@ top_left = max_loc
 bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
 output = cv2.rectangle(haystack, top_left, bottom_right, (0, 0, 255), 3)
 
-cv2.imshow('Haystack', grayhaystack)
-cv2.imshow('Result', result)
+
+# # Show haystack gray
+# cv2.imshow('Haystack', grayhaystack)
+
+# # Show transformed image
+# cv2.imshow('Result', result)
+
+# Show image with 'needle' found (if not found there will be no bounding box)
 cv2.imshow('output', output)
 cv2.waitKey(0)
 
+# Write output image
 cv2.imwrite('haystack_needle_scen_02_02.png', output)
 
 # Might need different core for when it turns red
