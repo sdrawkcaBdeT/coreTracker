789d2918d5a620845345485153c083073c07a6e2
sdrawkcaBdeT <sdrawkcabdet@gmail.com>
2023-09-13 15:55:45 -0500

    adjusting for continuous running... error on finding an awakening

--- omega_strikers/awakeningPool/object_detection_in_games.py
+++ omega_strikers/awakeningPool/object_detection_in_games.py
@@ -2,6 +2,8 @@ import cv2 as cv2
 import numpy as np
 import pandas as pd
 import time as time
+from omega_strikers.awakeningPool.environment import LIST_AWAKENINGS
+from omega_strikers.realtime.windowcapture import WindowCapture
 
 # Goal here is to support the awakening draft phase of the game by displaying what awakening are available in the pool still.
 
@@ -16,45 +18,6 @@ import time as time
 # In the second draft, the awakenings will be drawn from a pool of 24.
 # If there's a third draft, the awakenings will be drawn from a pool of 16.
 # If there's a fourth draft, the awakenings will be the remaining 8.
-
-# Here's a list of the currently in-play awakenings:
-
-LIST_AWAKENINGS = list((
-    'SPECIALIZED TRAINING',
-    'RAPID FIRE',
-    'MISSILE PROPULSION',
-    'TWIN DRIVE',
-    'SUPER SURGE',
-    'CAST TO LAST',
-    'MONUMENTALIST',
-    'PRIMETIME',
-    'HOTSHOT',
-    'BUILT DIFFERENT',
-    'EXTRA SPECIAL',
-    'TIMELESS CREATOR',
-    'TEMPO SWING',
-    'CHRONOBOOST',
-    'BIG FISH',
-    'HEAVY IMPACT',
-    'PEAK PERFORMANCE',
-    'BULK UP',
-    'AERIALS',
-    'QUICK STRIKE',
-    'REVERBERATION',
-    'GLASS CANNON',
-    'STACKS ON STACKS',
-    'DEADEYE',
-    'ORB DANCER',
-    'ORB PONDERER',
-    'ORB REPLICATOR',
-    'EGOIST',
-    'CATALYST',
-    'FIRE UP!',
-    'STAGGER SWAGGER',
-    'ONE-TWO PUNCH',
-    'PRIZE FIGHTER',
-    'PERFECT FORM',)
-)
 
 
 # I'm thinking about it like this-- let's ignore the 2 awakenings that go away at the start of the game, then
@@ -73,32 +36,30 @@ df_awakening_status = pd.DataFrame({'AWAKENING':LIST_AWAKENINGS})
 df_awakening_status['STATUS'] = 'AVAILABLE'
 awakenings_found = 0
 
+
+# initialize the WindowCapture class
+wincap = WindowCapture()
+
 # this must run continuously, so I'm putting it in a while loop that causes the code to run until at least the
 # first two awakenings drafts have been completed.
-while awakenings_found >= 18:    
-    time.sleep(15) # sleep for fifteen seconds before re-running    
+while awakenings_found <= 3:    
+    time.sleep(5) # sleep for fifteen seconds before re-running  
+    df_awakening_status # show awakening status before loop run  
     for awakening in LIST_AWAKENINGS:
     
-        haystack_filename="draft00"
-        haystack_filetype='.png'
-        haystack_filepath='omega_strikers/awakeningPool/img/haystacks/'
-
-        # Define haystack (image to find the needle in)
-        haystack = cv2.imread(haystack_filepath+haystack_filename+haystack_filetype)
+        # get an updated image of the game
+        screenshot = wincap.get_screenshot()
         # grayscale transformation
-        grayhaystack = cv2.cvtColor(haystack, cv2.COLOR_BGR2GRAY)
-        
+        grayhaystack = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+                
         # Define the needle 
+        # initialize the Vision class
         needle = cv2.imread('omega_strikers/awakeningPool/img/needle/{}.png'.format(awakening))
         # grayscale transformation
         grayneedle =cv2.cvtColor(needle, cv2.COLOR_BGR2GRAY)
 
-        # # Uncomment to show image
-        # cv2.imshow('Needle', needle)
-        # cv2.waitKey(0)
-
-        # # Uncomment to show image
-        # cv2.imshow('Haystack', haystack)
+        # # # Uncomment to show image
+        # cv2.imshow('Needle', grayhaystack)
         # cv2.waitKey(0)
 
         #methods available :[cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TMO_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
@@ -109,32 +70,28 @@ while awakenings_found >= 18:
         needle_h = needle.shape[0]
         top_left = max_loc
         bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
-        output = cv2.rectangle(haystack, top_left, bottom_right, (0, 0, 255), 3)
-        
-        # # Show haystack gray
-        # cv2.imshow('Haystack', grayhaystack)
-
-        # # Show transformed image
-        # cv2.imshow('Result', result)
-
-        # Show image with 'needle' found (if not found there will be no bounding box)
-        # cv2.imshow('output', output)
-        # cv2.waitKey(0)
-
-
-
+        output = cv2.rectangle(screenshot, top_left, bottom_right, (0, 0, 255), 3)
             
         if max_val >= .8:
             print('found.')
-            awakenings_found = awakenings_found + 1
-            df_awakening_status.loc[df_awakening_status['AWAKENING']==awakening,"STATUS"]="NOT AVAILABLE"        
+            
+            if df_awakening_status.loc[df_awakening_status['AWAKENING']==awakening,].STATUS=="NOT AVAILABLE":
+                print('found awakening has already been found before... going to next check.')
+                # awakenings_found = awakenings_found # don't chg no. of awk found if awk has been found before
+            else: # if newly found
+                print(print('found awakening is new... updating awakening status and count.'))
+                # update status to not available
+                df_awakening_status.loc[df_awakening_status['AWAKENING']==awakening,"STATUS"]="NOT AVAILABLE"
+                # add to the awakenings found counter
+                awakenings_found = awakenings_found + 1
+                       
+            
             # Write output image
-            cv2.imwrite('omega_strikers/awakeningPool/img/searched/found/{}.png'.format(awakening), output)
+            # cv2.imwrite('omega_strikers/awakeningPool/img/searched/found/{}.png'.format(awakening), output)
             
         else:
             print('not found')
         
 
-awakenings_found
-df_awakening_status        
+
 
